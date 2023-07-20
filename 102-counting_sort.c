6138f2282f5411185b6c9c0e91be60da5931e1af
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-07-21 00:17:59 +0200

    6

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -47,7 +47,7 @@ void counting_sort(int *array, size_t size)
 	}
 
 	for (n = 0; n < (m + 1); n++)
-		count[i] = 0;
+		count[n] = 0;
 	for (n = 0; n < (int)size; n++)
 		count[array[n]] += 1;
 	for (n = 0; i < (m + 1); n++)
